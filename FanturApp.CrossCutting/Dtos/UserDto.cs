05ba287c58e3344d177dea12226f3f0b5735f898
Xircodra <kusui.rukodora@gmail.com>
2022-07-05 19:59:31 -0300

    agregado campo de emailsub al userdto

--- FanturApp.CrossCutting/Dtos/UserDto.cs
+++ FanturApp.CrossCutting/Dtos/UserDto.cs
@@ -10,5 +10,7 @@
         public string Email { get; set; }
         public string Role { get; set; }
         public string PhoneNumber { get; set; }
+
+        public bool? EmailSubscription { get; set; }
     }
 }
